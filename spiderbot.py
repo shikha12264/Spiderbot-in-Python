e758731c05d37c31fbf650d14ef680e1cb02c97f
Shikha Singh <64529469+shikha12264@users.noreply.github.com>
2021-05-31 00:13:05 +0530

    Update spiderbot.py

--- spiderbot.py
+++ spiderbot.py
@@ -1,33 +1,66 @@
 import requests
 import validators
 import time
+from datetime import datetime, timedelta
 from urllib.parse import urljoin
-from bs4 import BeautifulSoup as bs # using allias bs
+from bs4 import BeautifulSoup as bs    # using allias bs
+from pymongo import MongoClient
+import dns
+
 url = "https://flinkhub.com/"
-lst = []
+
 #getting request from url
 lnk = requests.get(url)
-print("Flinkhub Link:",lnk.url)   #printing url
-print("Html Tags:",lnk.text)  #printing raw content of the specified url
-# Create a BeautifulSoup object
-soup = bs(lnk.text, 'html.parser') #parsing all html tags
-atags = soup.find_all("a", href=True)
-for i in atags: #finding all anchor tags
-    #retrieved all the links
-    atag = i["href"]
-    # Retrieve the href attribute from the anchor tag
-    if (atag != ' '):
-        #check if the href attribute is not an empty element and perform only if it has content in it
+
+print("Flinkhub Link:",lnk.url)
+#printing url
+
+print("Html Tags:",lnk.text)
+#printing raw content of the specified url
+
+client = MongoClient("mongodb+srv://test:test@cluster0.0del1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.get_database('shikha_db')
+records = db.web_scraper
+cnt = records.count_documents({})
+print('Count:',cnt)
+
+                                      #return datetime
         
-        rellnk = urljoin("https://flinkhub.com",i["href"])     
-        #If you want to know if an URL is absolute or relative link
-        #in order to join it with a base URL, use urllib.parse.urljoin anyway     
+#validate an URL using try & catch mechanism
+def Validlnk(sourceLink) :
+    try:
+        response = requests.get(sourceLink)
+        return True
+    except:
+        return False
 
-        if rellnk not in lst:
-            lst.append(rellnk)
+#function to check whether link already exists
+
+def alreadyExists(lnk):
+    try :
+        filtr =(list(records.find({"Link":lnk})))
+        if records.count_documents(filtr) > 0 :
+            return True                   
+    except :
+         return False
+    
+soup = bs(lnk.text, 'html.parser')
+atags = soup.find_all("a", href=True)
+
+for i in atags:   
+    atag = i["href"]    
+    if (atag != ' '):        
+            if (Validlnk(atag) and  not alreadyExists(atag)):             
+                records.insert_one({"Link": atag, "createdAt":datetime.today().replace(microsecond=0)})
+                print("Inserted link")
             
-print("List of valid links scraped:")
-print(lst)
+    else:
+        print("Issue")
+           
 
-print("Length of list i.e. number of valid links present:")
-print(len(lst))   #16 valid links with no repetition
+print("Done")
+z = records.count_documents({})
+print('Count after insertion:',z)
+
+
+            
