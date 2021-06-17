18203b4499c51d744150d8186a7c0a4d086f7c85
Shikha Singh <64529469+shikha12264@users.noreply.github.com>
2021-06-17 11:38:30 +0530

    Update spiderbot.py

--- spiderbot.py
+++ spiderbot.py
@@ -22,10 +22,10 @@ def db_conn():
     # testing collection connection
     # connecting to the collection (web_scraper) via db object (shikha_db)
     return collection
-    cnt = records.count_documents({})
-    if cnt >= 5000:
-        print("Maximum limit Reached")
-    print('Count before Insertion:',cnt)
+    ##cnt = records.count_documents({})
+    ##if cnt >= 5000:
+     ##   print("Maximum limit Reached")
+    ##print('Count before Insertion:',cnt)
     #count_documents is a method to get the count of documents under
     #collections(records)
 
@@ -33,12 +33,16 @@ def db_conn():
 def write_html(lnk):
     response = requests.get(lnk)                                         
     f = open("Html", "wb")                                               #Open a new file for writing
-    f.write(response.content)                                            #write content in the file
+    f.write(response.content)                                            #write content in the file "Html"
     f.close()
-
+    
+    
+ # check for already existing links
 def already_exists(url, collection):
-    data = collection.find({ "Link": url})
-    for d in data:                                                        # check for already existing links
+    al_lnk = collection.find({ "Link": url})                              #finding a particular document providing key value pair using find_one method
+        #finding all the documents using find method so this becomes an iterator and using type conversion to list we get
+        #a list of iterative objects 
+    for i in al_lnk:                                                       
         return True
     return False
 
@@ -47,61 +51,71 @@ def Insert_lnk(url, collection, source_Link):
         res = requests.get(url)
         if not already_exists(res.url, collection):
             soup = BeautifulSoup(res.content, 'html5lib')                       # Create a BeautifulSoup object& parsing all html tags using html.parser
-            random_file = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))        
-            with open('htmlFiles/{}.html'.format(random_file), 'w', encoding="utf-8") as file:
-                file.write(soup.prettify())
-            file_path = ((os.path.dirname(__file__)) + '\htmlFiles\{}.html'.format(random_file)) 
-            lnk = {
+              lnk = {
                 'Link': res.url,
                 'Source_Link': source_Link,
                 'Is_Crawled': False,
                 'Last_Crawl_Dt': '',
                 'Created_at': datetime.now()
             }
-            collection.insert_one(lnk)
+            collection.insert_one(lnk)                      
+                #insert_one method to insert one single document
+                #records.insert_one(link)
+                ##link2 = [{'cbjsab':'gh'},{'ahdujhn':'fg'}]
+                ##records.insert_many(link2)
+                ##insert_many method to insert multiple documents  
             print(res.url)
     except Exception as error:
         print(error)
         pass
     
- # function for links which hasnt been inserted
+ # function to keep a track of links which hasnt been inserted
 def notins_lnk(collection):
     links = collection.find({"$or": [{"Is_Crawled": False},{"Last_Crawl_Dt":{ "$lt": datetime.now() - timedelta(days=1)}}]})
     return links
 
 # function to update the document
 def modify_coll(document, collection):
+    #updateOne() method updates a first matched document within the collection based on the given query. 
+    # updating your document the value of the _id field remains unchanged. 
     collection.update_one({ "_id": document["_id"]}, { "$set": {
         'Is_Crawled': True,
         'Last_Crawl_Dt': datetime.now()
     }})
 
- #function to crawl links
+ #function to crawl all the links
 def crawl(document, collection):
     modify_coll(document, collection)
     response = getResponse(document["Link"], 10)
     links = getValidLinks(response)
     for link in links:
         if documentCount(collection) >=5000:
+            #crawling the links if it exceeds the recursion limit , raise an exception
             raise Exception("Maximum Limit Reached")
         Insert_lnk(link, collection, document["Link"])
 
 
 #function to validate links
 def getValidLinks(response):
-    links = []
-    soup = BeautifulSoup(response.content, 'html5lib')
-    href = soup.find_all('a', href = True)
-
-    for href in href:
-        if href.get('href').startswith("#") or href.get('href').startswith("tel:") or href.get('href').startswith("javascript:;") or href.get('href').startswith(" "):
+    links = []        
+    #creating an empty list links
+    soup = BeautifulSoup(response.content, 'html5lib')         # Create a BeautifulSoup object& parsing all html tags using html.parser
+    # finding all anchor tags having href attribute
+    href = soup.find_all('a', href = True)            # retrieved all the links having href attribute
+        #finding all the documents using find method so this becomes an iterator and using type conversion to list we get
+        #a list of iterative objects  
+    for i in href:
+        x = i.get('href')
+        if x.startswith("#") or href.get('href').startswith("tel:") or href.get('href').startswith("javascript:;") or href.get('href').startswith(" "):
             continue
-
-        if href.get('href').startswith("https://") or href.get('href').startswith("http://"):
+        #condition check for absolute urls
+        # if condition to check whether href attribute starts with these particular extensions so ignore them
+        if x.startswith("https://") or href.get('href').startswith("http://"):
             links.append(href.get('href'))
             continue
-
-        if href.get('href').startswith("/"):
+         # if condition to check whether href attribute starts with these particular extensions https or http append them to the links list
+        if x.startswith("/"):
+            # if condition to check whether href attribute starts with / i.e. checking whether it's a relative url
             temp_url = response.url
             temp_index = find_index(temp_url)
             newurl = temp_url.replace(temp_url[temp_index:],"") + href.get('href').replace("/","")
@@ -117,12 +131,14 @@ if __name__ == '__main__' :
         Insert_lnk(url, collection, "")
 
         while True:
-            cnt = collection.count_documents({})
-            if cnt >= 5000:
+            cnt = collection.count_documents({})   
+            #count_documents is a method to get the count of documents under
+             #collections(records)
+            if cnt >= 5000:                
                 print("Maximum limit Reached")
             documents = notins_lnk(collection)
             all_crawled = True
-            with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:           #ThreadPoolExecutor is library in python which is used to implement multithreading
                 futures = []
                 try:
                     for document in documents:
